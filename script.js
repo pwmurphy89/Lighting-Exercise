fdc59c3a74780bed21e1d8c0aa14a366bcce7b6f
pwmurphy89 <pwmurphy89@gmail.com>
2018-04-03 13:28:58 -0500

    correction

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-var lastName = 'Murphy';
-if (lastName.length => 7){
+const lastName = 'Murphy';
+if (lastName.length >= 7){
     console.log("I can't pronounce this last name!")
 }else{
    console.log(lastName);
